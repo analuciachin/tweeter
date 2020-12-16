2fc3e9c42610c523fd47b5b3f41703475823f8c8
analuciachin <ananime@gmail.com>
2020-12-16 04:17:31 +0000

    add hover effect on tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,13 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log('Client.js')
+  $('article.tweet').hover(function() {
+    $(this).children().find('.hide').removeClass('hide');
+  }, function() {
+    $(this).children('header').find('span:last-of-type').addClass('hide');
+  })
+});
